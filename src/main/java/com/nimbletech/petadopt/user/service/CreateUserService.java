408aa5632241cfda1ec88f34c5ef24f21cb1d316
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2025-09-27 20:11:04 +0300

    default user role for newly registered users

--- src/main/java/com/nimbletech/petadopt/user/service/CreateUserService.java
+++ src/main/java/com/nimbletech/petadopt/user/service/CreateUserService.java
@@ -4,6 +4,7 @@ import com.nimbletech.petadopt.Command;
 import com.nimbletech.petadopt.user.dto.CreateUserDto;
 import com.nimbletech.petadopt.user.dto.UserDto;
 import com.nimbletech.petadopt.user.mapper.UserMapper;
+import com.nimbletech.petadopt.user.model.Role;
 import com.nimbletech.petadopt.user.model.User;
 import com.nimbletech.petadopt.user.model.UserStatus;
 import com.nimbletech.petadopt.user.repository.UserRepository;
@@ -13,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,6 +33,7 @@ public class CreateUserService implements Command<CreateUserDto, UserDto> {
 
         String hashedPassword = passwordEncoder.encode(dto.getPassword());
         user.setPassword(hashedPassword);
+        user.setRoles(Set.of(Role.ROLE_USER));
 
         User saved = userRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED)

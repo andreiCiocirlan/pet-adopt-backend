226c33f86b2adfb580a1336f7ab142406829bdd7
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2025-09-27 15:11:26 +0300

    entity not found exception

--- src/main/java/com/nimbletech/petadopt/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/nimbletech/petadopt/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.nimbletech.petadopt.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -23,5 +24,12 @@ public class GlobalExceptionHandler {
         });
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }

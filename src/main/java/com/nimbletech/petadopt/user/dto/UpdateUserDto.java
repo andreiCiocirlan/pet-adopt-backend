74187a83a6c7aebc09dbd55ac724327eee922719
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2025-12-11 13:14:01 +0200

    fields validation for update user

--- src/main/java/com/nimbletech/petadopt/user/dto/UpdateUserDto.java
+++ src/main/java/com/nimbletech/petadopt/user/dto/UpdateUserDto.java
@@ -1,5 +1,8 @@
 package com.nimbletech.petadopt.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,7 +10,12 @@ import lombok.Data;
 @Data
 public class UpdateUserDto {
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
+
+    @NotBlank(message = "Phone is required")
     private String phone;
-    private String address;
+
+    private String address; // optional
 }

60d0e3a1f5b3161335057fe58a58814835592798
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2026-01-18 17:24:43 +0200

    remove autowired import

--- src/main/java/com/nimbletech/petadopt/config/SecurityConfig.java
+++ src/main/java/com/nimbletech/petadopt/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.nimbletech.petadopt.config;
 
 import com.nimbletech.petadopt.jwt.JwtRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;

241ddeff800d044df65231378b902c30b268bb98
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2026-01-19 14:54:13 +0200

    fix imports

--- src/main/java/com/nimbletech/petadopt/pet/controller/PetController.java
+++ src/main/java/com/nimbletech/petadopt/pet/controller/PetController.java
@@ -5,7 +5,6 @@ import com.nimbletech.petadopt.pet.model.AnimalType;
 import com.nimbletech.petadopt.pet.model.PetStatus;
 import com.nimbletech.petadopt.pet.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;

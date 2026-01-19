1b6d816e44f72e436f2f6a78b83d4c105a0a497b
Andrei Ciocirlan <andreiciocirlan93@gmail.com>
2026-01-19 13:35:33 +0200

    remove unused import

--- src/main/java/com/nimbletech/petadopt/pet/dto/PetSearchRequest.java
+++ src/main/java/com/nimbletech/petadopt/pet/dto/PetSearchRequest.java
@@ -2,7 +2,6 @@ package com.nimbletech.petadopt.pet.dto;
 
 import com.nimbletech.petadopt.pet.model.AnimalType;
 import com.nimbletech.petadopt.pet.model.PetStatus;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Pageable;
 
 public record PetSearchRequest(
